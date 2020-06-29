168fb33d01ebd611054c60ddfee6a8a4738d3918
taminhtu <taminhtu@gmail.com>
2020-06-29 15:52:48 +0700

    Initial commit

---
+++ src/main/java/com/tcb/hello/TcbApplication.java
@@ -0,0 +1,13 @@
+package com.tcb.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tcb/hello/TcbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcb.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
